b24079cf8f2e2a21755aa6d7e0290c4bb6c66c0f
MaironHenrique <mairon_fernandes13@hotmail.com>
2022-04-03 12:29:43 -0300

    Fizemos os metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,26 @@
 
 require_once("config.php");
 
-$root = new Usuario();
 
-$root->loadbyId(4);
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(4);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuario usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("root", "1004a!");
+
+echo $usuario;
+
 
 ?>
